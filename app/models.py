0c290ea9ecc0df43ad26f7190bcc11ad7c590fc1
Collins njau <collinsnjau39@gmail.com>
2018-03-30 21:48:41 +0300

    Add business model

--- app/models.py
+++ app/models.py
@@ -10,5 +10,30 @@ class Business(db.Model):
     category=db.Column(db.String(300))
     location=db.Column(db.String(300))
 
-    
+    def __init__(self,name,description,contact,category,location):
+        self.name=name
+        self.description=description
+        self.contact=contact
+        self.category=category
+        self.location=location
+
+
+    def save(self):
+        """save business to database"""
+        db.session.add(self)
+        db.session.commit()
+
+    @staticmethod
+    def get_all():
+        """returns all businesses"""
+        return Business.query.all()
+
+    def delete_business(self):
+        db.session.delete(self)
+        db.session.commit() 
+
+    def __repr__(self):
+        return "<Business: {}>".format(self.name)           
+
+
 
