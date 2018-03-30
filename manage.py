f417dbe29d2c4a345c1948284d41d8a737bd6d44
Collins njau <collinsnjau39@gmail.com>
2018-03-30 23:09:22 +0300

    Run the migration script

---
+++ manage.py
@@ -0,0 +1,17 @@
+import os
+
+from flask_script import Manager#manager tracks how the commands are run on the terminal
+from flask_migrate import Migrate, MigrateCommand#contains a set of migration commands
+from app import db,create_app
+from app import models
+
+config_name= os.getenv('APP_SETTINGS')
+app= create_app(config_name)
+
+migrate=Migrate(app,db)
+manager=Manager(app)
+
+manager.add_command('db', MigrateCommand)
+
+if __name__ == '__main__':
+    manager.run()
