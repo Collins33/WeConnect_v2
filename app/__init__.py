24afa040a386609019103b81ca1eab22312d33cd
Collins njau <collinsnjau39@gmail.com>
2018-03-30 19:44:24 +0300

    Create flask-api object and load configurations

--- app/__init__.py
+++ app/__init__.py
@@ -7,3 +7,16 @@ from instance.config import app_config
 #initialize sqlalchemy
 db=SQLAlchemy()
 
+def create_app(config_name):
+    """this method wraps creation of flask-api
+    object and returns it after loading the configurations"""
+
+    app=FlaskAPI(__name__, instance_relative_config=True)
+    app.config.from_object(app_config[config_name])
+    app.config.from_pyfile('config.py')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    #connect the db
+    db.init_app(app)
+
+    return app
