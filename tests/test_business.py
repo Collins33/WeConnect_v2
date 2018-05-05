eb072f7696e1048567962e40cabc35780f9c40c7
Collins Njau <collinsnjau39@gmail.com>
2018-05-05 17:51:41 +0300

    Remove commented out code

--- tests/test_business.py
+++ tests/test_business.py
@@ -110,81 +110,12 @@ class BusinessTestCase(unittest.TestCase):
         edit_response=self.client().put('/api/v2/businesses/30',headers=dict(Authorization="Bearer "+ access_token),data=self.secondBusiness)
         self.assertEqual(edit_response.status_code,404)    
              
-        
-        
-
-
-        
-        
-        
-        
-            
-
-    # def test_business_creation_for_unregistered_user(self):
-    #     result=self.client().post('/api/v2/businesses', data=self.business_test)
-    #     self.assertEqual(result.status_code,401)    
-
-#     def test_business_creation_without_all_details(self):
-#         response=self.client().post('/api/v2/businesses', data=self.business_test)
-#         self.assertEqual(response.status_code,400)
-
-#     def test_api_can_get_all_businesses(self):
-#         response=self.client().post('/api/v2/businesses', data=self.business)
-#         self.assertEqual(response.status_code,201)
-#         res=self.client().get('/api/v2/businesses', data=self.business)
-#         self.assertEqual(res.status_code,200)
-
-#     def test_api_can_get_individual_business(self):
-#         response=self.client().post('/api/v2/businesses', data=self.business)
-#         self.assertEqual(response.status_code,201)
-#         #convert the response to json
-#         result_in_json=json.loads(response.data.decode('utf-8').replace("'", "\""))
-
-#         result=self.client().get('/api/v2/businesses/{}'.format(result_in_json['id']))
-#         self.assertEqual(result.status_code,200)
-
-#     def test_api_can_delete_business(self):
-#         response=self.client().post('/api/v2/businesses', data=self.business)
-#         self.assertEqual(response.status_code,201)
-
-#         result_in_json=json.loads(response.data.decode('utf-8').replace("'", "\""))
-#         result=self.client().delete('/api/v2/businesses/{}'.format(result_in_json['id']))
-#         self.assertEqual(result.status_code,200)
-
-#     def test_api_can_edit_business(self):
-#         response=self.client().post('/api/v2/businesses', data=self.business)
-#         self.assertEqual(response.status_code,201)
-
-#         result_in_json=json.loads(response.data.decode('utf-8').replace("'", "\""))
-
-#         result=self.client().put('/api/v2/businesses/1',data=self.business_edit)
-#         self.assertEqual(result.status_code,200)
-
-#     def test_api_can_get_businesses_location(self):
-#         response=self.client().post('/api/v2/businesses', data=self.business)
-#         self.assertEqual(response.status_code,201)
-#         #convert the response to json
-#         result_in_json=json.loads(response.data.decode('utf-8').replace("'", "\""))
-
-#         result=self.client().get('/api/v2/businesses/{}'.format(result_in_json['location']))
-#         self.assertEqual(result.status_code,200)
-
-#     def test_api_can_get_business_category(self):
-#         response=self.client().post('/api/v2/businesses', data=self.business)
-#         self.assertEqual(response.status_code,201)
-#         #convert the response to json
-#         result_in_json=json.loads(response.data.decode('utf-8').replace("'", "\""))
-
-#         result=self.client().get('/api/v2/businesses/{}'.format(result_in_json['category']))
-#         self.assertEqual(result.status_code,200)
-
-
-        def tearDown(self):
-            """connect to current context
-            and drop all tables"""
-            with self.app.app_context():
-                db.session.remove()
-                db.drop_all()
+    def tearDown(self):
+        """connect to current context
+        and drop all tables"""
+        with self.app.app_context():
+            db.session.remove()
+            db.drop_all()
 
 
 
