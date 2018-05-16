3e392957c2bf76dcbb9836f1d96d64f364e0eb11
Collins Njau <collinsnjau39@gmail.com>
2018-05-16 15:46:42 +0300

    Add comment to config

--- instance/config.py
+++ instance/config.py
@@ -14,7 +14,7 @@ class DevelopmentConfig(Config):
     DEBUG = True
 
 class TestingConfig(Config):
-    """Configurations for Testing, with a separate test database."""
+    """Configurations for Testing, with a separate test database. Used during testing"""
     TESTING = True
     SQLALCHEMY_DATABASE_URI = 'postgresql://collinsnjau:wrestlemania34@localhost/test_db'
     DEBUG = True
