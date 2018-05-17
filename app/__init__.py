06cee35d3ad9d0636205dc99886b952caadce76c
Collins Njau <collinsnjau39@gmail.com>
2018-05-17 11:33:56 +0300

    Add route for filtering by category

--- app/__init__.py
+++ app/__init__.py
@@ -293,7 +293,39 @@ def create_app(config_name):
         response=jsonify({"message":message,"status_code":400})
         #404 if business does not exist
         response.status_code=400
-        return response    
+        return response
+    
+        
+    @app.route('/api/v2/businesses/<string:category>', methods=['GET'])
+    def filter_category(category):
+        """get business based on category"""
+
+        businesses=Business.get_business_category(category)
+        business_category=[]
+
+        if not businesses:
+            message="No business in that category"
+            response=jsonify({"message":message,"status_code":404})
+            response.status_code=404
+            return response
+        else:
+            for business in businesses:
+                obj={
+                    'id':business.id,
+                    'name':business.name,
+                    'description':business.description,
+                    'location':business.location,
+                    'contact':business.contact,
+                    'category':business.category
+                }
+                business_category.append(obj)
+
+            response=jsonify(business_category)
+            response.status_code=200
+            return response 
+
+
+    
     
     @app.route('/api/v2/businesses/<int:id>/reviews', methods=['POST'])
     def add_review(id):
