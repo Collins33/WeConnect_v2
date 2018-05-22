41d6c485cd35a3dd96f31d6b11fcc97e3bdcd266
Collins Njau <collinsnjau39@gmail.com>
2018-05-22 09:21:45 +0300

    Add pylint

--- tests/test_reviews.py
+++ tests/test_reviews.py
@@ -1,81 +1,81 @@
-import unittest
 import json
-from app import db,create_app
+from app import db, create_app
 from tests.base import BaseTestHelper
 
+
 class ReviewsTestCase(BaseTestHelper):
-    #reviews url
-    review_url='api/v2/businesses/{}/reviews'
+    # reviews url
+    review_url = 'api/v2/businesses/{}/reviews'
     """class for testing reviews"""
     def setUp(self):
         """initilize the app and 
         set test variables"""
-        self.app=create_app(config_name="testing")
-        self.client=self.app.test_client
-        self.review={"opinion":"Good business with good food","rating":5}
-        self.wrong_review={"rating":5}
-        self.wrong_rating={"opinion":"Good business with good food"}
-        self.business={'name':'crastycrab','description':'Fastfood','contact':'0702848032','category':'fastfood','location':'atlantic'}
-        #bind app to the current context
+        self.app = create_app(config_name="testing")
+        self.client = self.app.test_client
+        self.review = {"opinion": "Good business with good food", "rating": 5}
+        self.wrong_review = {"rating": 5}
+        self.wrong_rating = {"opinion": "Good business with good food"}
+        self.business = {'name': 'crastycrab', 'description': 'Fastfood', 'contact': '0702848032', 'category': 'fastfood', 'location': 'atlantic'}
+        # bind app to the current context
         with self.app.app_context():
-            #create db tables
+            # create db tables
             db.create_all()
 
     def add_business(self):
-        self.register_user("collins.muru@andela.com","123test","123test")
-        result=self.login_user("collins.muru@andela.com","123test","123test")
-        #get the access token
-        access_token=json.loads(result.data.decode())['access_token']
-        #add the access token to the header
-        response=self.client().post('/api/v2/businesses',headers=dict(Authorization="Bearer "+ access_token) ,data=self.business)
+        self.register_user("collins.muru@andela.com", "123test", "123test")
+        result = self.login_user("collins.muru@andela.com", "123test", "123test")
+        # get the access token
+        access_token = json.loads(result.data.decode())['access_token']
+        # add the access token to the header
+        response = self.client().post('/api/v2/businesses',  headers=dict(Authorization="Bearer " + access_token), data=self.business)
         return response        
 
     def test_review_creation(self):
-        #first add the business
+        # first add the business
         self.add_business()
-        #add the review for the business
-        result=self.client().post(ReviewsTestCase.review_url.format('1'), data=self.review)
-        self.assertEqual(result.status_code,201)#review created
+        # add the review for the business
+        result = self.client().post(ReviewsTestCase.review_url.format('1'), data= self.review)
+        self.assertEqual(result.status_code, 201)# review created
         self.assertIn("succesfully added the review", str(result.data))
 
     def test_review_get_all(self):
         self.add_business()
         self.client().post(ReviewsTestCase.review_url.format('1'), data=self.review)
-        #get reviews for the particular business
-        result=self.client().get(ReviewsTestCase.review_url.format('1'))
-        self.assertEqual(result.status_code,200)#reviews successfully created
+        # get reviews for the particular business
+        result = self.client().get(ReviewsTestCase.review_url.format('1'))
+        self.assertEqual(result.status_code, 200)# reviews successfully created
 
     def test_review_not_exist(self):
         self.add_business()
-        #get reviews without adding the reviews
-        result=self.client().get(ReviewsTestCase.review_url.format('1'))
-        self.assertEqual(result.status_code,404)
+        # get reviews without adding the reviews
+        result = self.client().get(ReviewsTestCase.review_url.format('1'))
+        self.assertEqual(result.status_code, 404)
 
     def test_add_review_business_not_exist(self):
-        #post review for a business that does not exist
-        result=self.client().post(ReviewsTestCase.review_url.format('10'), data=self.review)
-        self.assertEqual(result.status_code,404)
+        # post review for a business that does not exist
+        result = self.client().post(ReviewsTestCase.review_url.format('10'), data=self.review)
+        self.assertEqual(result.status_code, 404)
         self.assertIn('cannot add review business that does not exist', str(result.data))
 
     def test_get_review_business_not_exist(self):
         self.add_business()
         self.client().post(ReviewsTestCase.review_url.format('1'), data=self.review)
-        #get review for a business that does not exist
-        result=self.client().get(ReviewsTestCase.review_url.format('5'))
-        self.assertEqual(result.status_code,404)
+        # get review for a business that does not exist
+        result = self.client().get(ReviewsTestCase.review_url.format('5'))
+        self.assertEqual(result.status_code, 404)
 
     def test_add_empty_opinion(self):
-        #first add the business
+        # first add the business
         self.add_business()
-        result=self.client().post(ReviewsTestCase.review_url.format('1'), data=self.wrong_review)
-        self.assertEqual(result.status_code,400)#bad request
+        result = self.client().post(ReviewsTestCase.review_url.format('1'), data=self.wrong_review)
+        self.assertEqual(result.status_code, 400)
         self.assertIn("make sure the opinion and rating are included", str(result.data))
 
     def test_add_empty_rating(self):
-        #first add the business
+        # first add the business
         self.add_business()
-        result=self.client().post(ReviewsTestCase.review_url.format('1'), data=self.wrong_review)
-        self.assertEqual(result.status_code,400)#bad request
+        result = self.client().post(ReviewsTestCase.review_url.format('1'), data=self.wrong_review)
+        self.assertEqual(result.status_code, 400)
         self.assertIn("make sure the opinion and rating are included", str(result.data))
                  
     def tearDown(self):
