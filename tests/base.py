6c673af2acdeaaa25ebf3b93f021001cc19c0e58
Collins Njau <collinsnjau39@gmail.com>
2018-05-22 09:25:40 +0300

    Fix pylint issues in base file

--- tests/base.py
+++ tests/base.py
@@ -1,30 +1,32 @@
 import unittest
 from app import create_app
+from app import db
+
 
 class BaseTestHelper(unittest.TestCase):
     def setUp(self):
-        self.app=create_app(config_name="testing")
-        self.client=self.app.test_client
+        self.app = create_app(config_name="testing")
+        self.client = self.app.test_client
         with self.app.app_context():
-            #create db tables
+            # create db tables
             db.create_all()
 
-    def register_user(self,email,password,confirm_password):
+    def register_user(self, email, password, confirm_password):
         """this method will register a user"""
-        user_data={
-            'email':email,
-            'password':password,
-            'confirm_password':confirm_password
+        user_data = {
+            'email': email,
+            'password': password,
+            'confirm_password': confirm_password
         }
-        return self.client().post('/api/v2/auth/registration',data=user_data)
+        return self.client().post('/api/v2/auth/registration', data=user_data)
 
-    def login_user(self,email,password,confirm_password):
-        user_data={
-            'email':email,
-            'password':password,
-            'confirm_password':confirm_password
+    def login_user(self, email, password, confirm_password):
+        user_data = {
+            'email': email,
+            'password': password,
+            'confirm_password': confirm_password
         }
-        return self.client().post('/api/v2/auth/login',data=user_data)
+        return self.client().post('/api/v2/auth/login', data=user_data)
 
     def tearDown(self):
         """connect to current context
