5c451a2feff38d657a9a15e3f165b1ba635cd16c
Collins Njau <collinsnjau39@gmail.com>
2018-05-23 09:43:31 +0300

    Fix config file

--- manage.py
+++ manage.py
@@ -4,7 +4,6 @@ from flask_migrate import Migrate, MigrateCommand
 from app import db, create_app
 
 config_name = os.getenv('APP_SETTINGS')
-
 app = create_app(config_name)
 
 migrate = Migrate(app, db)
